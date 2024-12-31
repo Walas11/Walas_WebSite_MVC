fb2defc36c79437d50e2a4eb2f10a2465784cc9c
Sebastian Acero Leon <NG28H@techtitute.com>
2024-12-31 13:17:31 -0500

    *** CREATE PROJECT ***

---
+++ Walas_WebSite_MVC/Walas_WebSite_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Walas_WebSite_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
