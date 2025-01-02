cb0c737407bcdba1363785de99961606eaa9b30b
Sebastian Acero Leon <NG28H@techtitute.com>
2025-01-02 15:28:57 -0500

    feat: Se incluye el bootstrap, pasos para incluir cualquier bootstrap:
    1. Descargar Boostrap aqui: https://themewagon.com/theme-framework/bootstrap-5/
    2. Insertar los js y css en la carpeta wwwroot
    3. pegar todo el contenido del bootstrap en la carpeta wwwroot
    4. Incluir el index en la vista _Layout.cshtml y cambiar el head por el del Index del bootstrap y despues del footer los Js
    5. Cambiar las rutas que vienen por defecto en el bootstrap hay que cambiarlo por "~/" NO OLVIDAR EL SLASH
    6. inlcuir todo en el _Layout para que todo quede por defecto ahi

---
+++ Walas_WebSite_MVC/Walas_WebSite_MVC/Controllers/WebSiteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Walas_WebSite_MVC.Controllers
+{
+    public class WebSiteController : Controller
+    {
+        // GET: WebSiteController
+        public ActionResult WebSite()
+        {
+            return View();
+        }
+    }
+}
